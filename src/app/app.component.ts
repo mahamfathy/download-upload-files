49cdb6c611aafb48a368176c3f190e8ff5c57a69
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-09 23:50:19 +0300

    Committing local changes before merging 'main' branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +14,52 @@ import { HttpClientModule } from '@angular/common/http';
 export class AppComponent {
   title = 'download and upload Files';
   inputData: string = '';
-  fileUrl = ' https://api.escuelajs.co/api/v1/files/';
-  uploadFile(event: any) {}
+  FileUrl = 'https://api.escuelajs.co/api/v1/files/';
+  uploadFileNames: string[] = [];
+
+  constructor(private http: HttpClient) {}
+  uploadFile(event: any) {
+    const file = event.currentTarget.files[0];
+    console.log(file);
+    if (file.type === 'image/png' && file.size < 2000000) {
+      const formObj = new FormData();
+      formObj.append('file', file);
+      debugger;
+      this.http
+        .post('https://api.escuelajs.co/api/v1/files/upload', formObj)
+        .subscribe((res: any) => {
+          debugger;
+          console.log(res);
+
+          this.uploadFileNames.push(res.filename);
+          console.log(this.FileUrl + res.filename);
+        });
+      this.inputData = '';
+    } else {
+      if (file.size > 2000000) {
+        alert('file size must be less than 2 MB');
+      } else {
+        alert('only files with png extension');
+      }
+    }
+  }
 }
+
+// uploadFile(event: any) {
+//   debugger;
+//   const file = event.currentTarget.files[0];
+//   console.log(file);
+//   if (file.type === 'image/jpg' && file.size < 2000000) {
+//     const formObj = new FormData();
+//     formObj.append('file', file);
+//     console.log(formObj);
+//     this.http
+//       .post('https://api.escuelajs.co/api/v1/files/upload', formObj)
+//       .subscribe((res: any) => {
+//         debugger;
+//         console.log(res);
+//         this.uploadFileNames.push(res.fileName);
+//       });
+//   }
+// }
+// }
