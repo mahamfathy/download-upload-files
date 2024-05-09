eb08e6c0f0739a12faa4541d07343b9cab43a4f4
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-09 21:12:08 +0300

    adding fake API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'downloadUploadFiles';
+  title = 'download and upload Files';
+  inputData: string = '';
+  fileUrl = ' https://api.escuelajs.co/api/v1/files/';
+  uploadFile(event: any) {}
 }
